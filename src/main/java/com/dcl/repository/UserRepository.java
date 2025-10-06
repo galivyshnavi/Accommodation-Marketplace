ff4de15a40bf8a96f402e106a56126ddd2d30823
galivyshnavi <galivyshnavi9@gmail.com>
2025-10-06 11:07:07 +0530

    commit

--- src/main/java/com/dcl/repository/UserRepository.java
+++ src/main/java/com/dcl/repository/UserRepository.java
@@ -2,8 +2,9 @@ package com.dcl.repository;
 
 import com.dcl.entity.User;
 
+
 import java.util.UUID;
 
-public class UserRepository extends JpaRepository<User, UUID> {
+public class UserRepository  {
 
 }
